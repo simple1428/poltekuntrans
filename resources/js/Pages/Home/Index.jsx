8ffbcdf5f064b27ccea516291173f8453620d352
MuhammadNiam529 <Niamulmujib@gmail.com>
2024-06-01 14:24:39 +0700

    first test

---
+++ resources/js/Pages/Home/Index.jsx
@@ -0,0 +1,10 @@
+import { Head } from "@inertiajs/react";
+import React from "react";
+
+export default function Index() {
+    return (
+        <>
+            <Head title="Home" />
+        </>
+    );
+}
--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+use App\Http\Controllers\HomeController\Index as HomeController;
+
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
