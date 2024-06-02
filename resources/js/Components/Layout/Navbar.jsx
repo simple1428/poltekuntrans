eb34bcd7b996ebde793c44df2ebb0846bdb6b1f2
simple1428 <misbahudin1428@gmail.com>
2024-06-02 12:51:08 +0700

    first test in another pc

--- resources/js/Components/Layout/Navbar.jsx
+++ resources/js/Components/Layout/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     const active = "font-semibold";
     const inactive = "font-normall";
     return (
-        <header className=" sticky  top-5 ">
+        <header className=" sticky  top-5 z-[990]">
             <div className="flex w-full justify-between container items-center  shadow-md mx-auto bg-white  px-[32px] py-4 rounded-[10px]">
                 <h1 className="text-2xl font-semibold ">Poltekun Trans</h1>
                 <div className="flex justify-evenly gap-4 items-center text-xl">
